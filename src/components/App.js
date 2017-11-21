410da5e76e602a22a3fa4083313d59908ef38f94
John Shea <sheajohnh@gmail.com>
2017-11-20 22:56:22 -0500

    Implement mapStateToProps in src/components/App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 
 class App extends Component {
   render() {
+    console.log('Props', this.props)
     return (
       <div>
         Hello World!
       </div>
-    );
+    )
   }
 }
 
-export default App;
+function mapStateToProps (calendar) {
+  const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+
+  return {
+    calendar: dayOrder.map((day) => ({
+      day,
+      meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+        meals[meal] = calendar[day][meal]
+          ? calendar[day][meal]
+          : null
+
+        return meals
+      }, {})
+    }))
+  }
+}
+
+export default connect(mapStateToProps)(App)
